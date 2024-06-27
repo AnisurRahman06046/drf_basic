f68dcaf493c486613b6f5e0f25bdfe1a99a5cc0f
AnisurRahman06046 <anisurrahman06046@gmail.com>
2024-06-28 02:59:06 +0600

    model serializer

--- products/serializers.py
+++ products/serializers.py
@@ -2,23 +2,30 @@ from rest_framework import serializers
 from . models import Products
 
 
-
+# model serializer
+class ProductsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Products
+        fields = ['title','qty','description']
 
 
 # the below serializer method is raw method to understand the concept. for production we wiil use model serializer which is coded above
-class ProductsSerializer(serializers.Serializer):
-    title = serializers.CharField(max_length=75)
-    qty = serializers.IntegerField()
-    description = serializers.CharField()
-    # date = serializers.DateTimeField()
+
+
+
+# class ProductsSerializer(serializers.Serializer):
+#     title = serializers.CharField(max_length=75)
+#     qty = serializers.IntegerField()
+#     description = serializers.CharField()
+#     # date = serializers.DateTimeField()
     
-    def create(self,validated_data):
-        return Products.objects.create(**validated_data)
+#     def create(self,validated_data):
+#         return Products.objects.create(**validated_data)
     
-    def update(self, instance, validated_data):
-        # return super().update(instance, validated_data)
-        instance.title = validated_data.get('title',instance.title)
-        instance.qty = validated_data.get('qty',instance.qty)
-        instance.description = validated_data.get('description',instance.description)
-        instance.save()
-        return instance
+#     def update(self, instance, validated_data):
+#         # return super().update(instance, validated_data)
+#         instance.title = validated_data.get('title',instance.title)
+#         instance.qty = validated_data.get('qty',instance.qty)
+#         instance.description = validated_data.get('description',instance.description)
+#         instance.save()
+#         return instance
