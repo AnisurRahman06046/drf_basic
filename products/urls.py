997290aceb195f7f896322adbbd8b3300163e973
AnisurRahman06046 <anisurrahman06046@gmail.com>
2024-06-28 04:25:36 +0600

    class based view

--- products/urls.py
+++ products/urls.py
@@ -1,14 +1,19 @@
 from django.urls import path
-from . import views
+from products import views
 urlpatterns = [
 
     # path("all/",views.allProducts,name='allProducts'),
-    path("add/",views.create_product,name='add'),
-    path("create/",views.fetchProducts,name='create'),
-    path("get-data/",views.fetchProducts,name='products'),
-    path("get-data/<str:title>/",views.fetchProducts,name='product'),
-    path("edit/<str:title>/",views.fetchProducts,name='product'),
-    path("remove/<str:title>/",views.fetchProducts,name='remove'),
-    path("update/<str:title>/",views.updateProduct,name='update'),
-    path("delete/<str:title>/",views.deleteProduct,name='delete'),
+    # path("add/",views.create_product,name='add'),
+    path("create/",views.ProductsAPI.as_view(),name='create'),
+    path("get-data/",views.ProductsAPI.as_view(),name='ProductsAPI'),
+    path("get-data/<str:title>/",views.ProductsAPI.as_view(),name='product'),
+    path("edit/<str:title>/",views.ProductsAPI.as_view(),name='product'),
+    path("remove/<str:title>/",views.ProductsAPI.as_view(),name='remove'),
+    # path("create/",views.fetchProducts,name='create'),
+    # path("get-data/",views.fetchProducts,name='products'),
+    # path("get-data/<str:title>/",views.fetchProducts,name='product'),
+    # path("edit/<str:title>/",views.fetchProducts,name='product'),
+    # path("remove/<str:title>/",views.fetchProducts,name='remove'),
+    # path("update/<str:title>/",views.updateProduct,name='update'),
+    # path("delete/<str:title>/",views.deleteProduct,name='delete'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,54 +1,104 @@
 from django.shortcuts import render,HttpResponse
 from .models import Products
 from .serializers import ProductsSerializer
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
-from django.views.decorators.csrf import csrf_exempt
-import io 
-from django.http import JsonResponse
-from rest_framework.decorators import api_view
+# from rest_framework.renderers import JSONRenderer
+# from rest_framework.parsers import JSONParser
+# from django.views.decorators.csrf import csrf_exempt
+# import io 
+# from django.http import JsonResponse
+# from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
 # Create your views here.
 
-
-
-# this is api view
-@api_view(['GET','POST','PATCH','DELETE'])
-def fetchProducts(request,title=None):
-    if request.method=='GET':
-        if title is not None:
-            query  = Products.objects.get(title=title)
-            serializer = ProductsSerializer(query)
+# class based view
+class ProductsAPI(APIView):
+    
+    def get(self,request,title=None,format=None):
+        # fet single and multiple products
+        if request.method=='GET':
+            if title is not None:
+                query  = Products.objects.get(title=title)
+                serializer = ProductsSerializer(query)
+                return Response(serializer.data,status=200)
+            data = Products.objects.all()
+            serializer = ProductsSerializer(data,many=True)
             return Response(serializer.data,status=200)
-        data = Products.objects.all()
-        serializer = ProductsSerializer(data,many=True)
-        return Response(serializer.data,status=200)
     
-    if request.method=='POST':
-        result = ProductsSerializer(data=request.data)
-        if result.is_valid():
-            result.save()
-            return Response(result.data,status=201)
-        return Response({'msg':"Failed"},status=400)
+    # create product
+    def post(self,request,format=None):
+        if request.method=='POST':
+            result = ProductsSerializer(data=request.data)
+            if result.is_valid():
+                result.save()
+                return Response(result.data,status=201)
+            return Response({'msg':"Failed"},status=400)
     
-    if request.method=='PATCH':
-        queryData = Products.objects.get(title=title)
-        result = ProductsSerializer(queryData,data=request.data,partial=True)
-        if result.is_valid():
-            result.save()
-            return Response(result.data,status=200)
-        return Response({'msg':'Failed to update'},status=400)
     
-    if request.method=='DELETE':
-        queryData = Products.objects.get(title=title)
+    # update product
+    def patch(self,request,title,format=None):
+        if request.method=='PATCH':
+            queryData = Products.objects.get(title=title)
+            result = ProductsSerializer(queryData,data=request.data,partial=True)
+            if result.is_valid():
+                result.save()
+                return Response(result.data,status=200)
+            return Response({'msg':'Failed to update'},status=400)
+    
+    
+    # delete product
+    def delete(self,request,title,format=None):
+        if request.method=='DELETE':
+            queryData = Products.objects.get(title=title)
+            
+            queryData.delete()
+            return Response({'msg':'deleted'})
+
+
+# ------------------- function based api view-------------------
+# this is api view
+# @api_view(['GET','POST','PATCH','DELETE'])
+# def fetchProducts(request,title=None):
+#     # fet single and multiple products
+#     if request.method=='GET':
+#         if title is not None:
+#             query  = Products.objects.get(title=title)
+#             serializer = ProductsSerializer(query)
+#             return Response(serializer.data,status=200)
+#         data = Products.objects.all()
+#         serializer = ProductsSerializer(data,many=True)
+#         return Response(serializer.data,status=200)
+    
+#     # create product
+#     if request.method=='POST':
+#         result = ProductsSerializer(data=request.data)
+#         if result.is_valid():
+#             result.save()
+#             return Response(result.data,status=201)
+#         return Response({'msg':"Failed"},status=400)
+    
+    
+#     # update product
+#     if request.method=='PATCH':
+#         queryData = Products.objects.get(title=title)
+#         result = ProductsSerializer(queryData,data=request.data,partial=True)
+#         if result.is_valid():
+#             result.save()
+#             return Response(result.data,status=200)
+#         return Response({'msg':'Failed to update'},status=400)
+    
+    
+#     # delete product
+#     if request.method=='DELETE':
+#         queryData = Products.objects.get(title=title)
         
-        queryData.delete()
-        return Response({'msg':'deleted'})
+#         queryData.delete()
+#         return Response({'msg':'deleted'})
     
     
     
 
-
+# ********************** Raw code ******************************************
 # def allProducts(request):
 #     # complex dataset
 #     products = Products.objects.all()
@@ -79,47 +129,47 @@ def fetchProducts(request,title=None):
 #             return HttpResponse('parsed successfully')
 #         else:
 #             return HttpResponse("Failed")
-@csrf_exempt
-def create_product(request):
-    if request.method == 'POST':
-        data = request.body
-        streamed = io.BytesIO(data)
-        parsedData = JSONParser().parse(streamed)
-        serializer = ProductsSerializer(data=parsedData)
+# @csrf_exempt
+# def create_product(request):
+#     if request.method == 'POST':
+#         data = request.body
+#         streamed = io.BytesIO(data)
+#         parsedData = JSONParser().parse(streamed)
+#         serializer = ProductsSerializer(data=parsedData)
         
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse({'status': 'success', 'message': 'Product created successfully'}, status=201)
-        else:
-            return JsonResponse({'status': 'error', 'errors': serializer.errors}, status=400)
-    return JsonResponse({'status': 'error', 'message': 'Invalid request method'}, status=405)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return JsonResponse({'status': 'success', 'message': 'Product created successfully'}, status=201)
+#         else:
+#             return JsonResponse({'status': 'error', 'errors': serializer.errors}, status=400)
+#     return JsonResponse({'status': 'error', 'message': 'Invalid request method'}, status=405)
         
 
-@csrf_exempt  
-def updateProduct(request,title):
-    if request.method=='PUT':
-        # find data from model
-        querySet = Products.objects.get(title=title)
-        data = request.body 
-        streamed = io.BytesIO(data)
-        parsed = JSONParser().parse(streamed)
-        serialized = ProductsSerializer(querySet,data=parsed,partial=True)
+# @csrf_exempt  
+# def updateProduct(request,title):
+#     if request.method=='PUT':
+#         # find data from model
+#         querySet = Products.objects.get(title=title)
+#         data = request.body 
+#         streamed = io.BytesIO(data)
+#         parsed = JSONParser().parse(streamed)
+#         serialized = ProductsSerializer(querySet,data=parsed,partial=True)
         
-        if serialized.is_valid():
-            serialized.save()
-            return JsonResponse({'status': 'success', 'message': 'Product updated successfully'}, status=201)
-        else:
-            return JsonResponse({'status': 'error', 'errors': serialized.errors}, status=400)
-    return JsonResponse({'status': 'error', 'message': 'Invalid request method'}, status=405)
+#         if serialized.is_valid():
+#             serialized.save()
+#             return JsonResponse({'status': 'success', 'message': 'Product updated successfully'}, status=201)
+#         else:
+#             return JsonResponse({'status': 'error', 'errors': serialized.errors}, status=400)
+#     return JsonResponse({'status': 'error', 'message': 'Invalid request method'}, status=405)
 
 
 
-@csrf_exempt 
-def deleteProduct(request,title):
-    if request.method=='DELETE':
-        query = Products.objects.get(title=title)
-        # streamed = io.BytesIO(query)
-        # parsed = JSONParser().parse(streamed)
-        query.delete()
-        return JsonResponse({'status': 'success', 'message': 'Product deleted successfully'}, status=201)
+# @csrf_exempt 
+# def deleteProduct(request,title):
+#     if request.method=='DELETE':
+#         query = Products.objects.get(title=title)
+#         # streamed = io.BytesIO(query)
+#         # parsed = JSONParser().parse(streamed)
+#         query.delete()
+#         return JsonResponse({'status': 'success', 'message': 'Product deleted successfully'}, status=201)
         
