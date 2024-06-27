e676dcd9af02c618135b4fcb8e370a0597bf2bb8
AnisurRahman06046 <anisurrahman06046@gmail.com>
2024-06-28 04:03:24 +0600

    api_view done

--- products/urls.py
+++ products/urls.py
@@ -2,9 +2,13 @@ from django.urls import path
 from . import views
 urlpatterns = [
 
-    path("all/",views.allProducts,name='allProducts'),
+    # path("all/",views.allProducts,name='allProducts'),
     path("add/",views.create_product,name='add'),
-    path("single/<str:title>/",views.singleProduct,name='product'),
+    path("create/",views.fetchProducts,name='create'),
+    path("get-data/",views.fetchProducts,name='products'),
+    path("get-data/<str:title>/",views.fetchProducts,name='product'),
+    path("edit/<str:title>/",views.fetchProducts,name='product'),
+    path("remove/<str:title>/",views.fetchProducts,name='remove'),
     path("update/<str:title>/",views.updateProduct,name='update'),
     path("delete/<str:title>/",views.deleteProduct,name='delete'),
 ]
--- products/views.py
+++ products/views.py
@@ -6,25 +6,67 @@ from rest_framework.parsers import JSONParser
 from django.views.decorators.csrf import csrf_exempt
 import io 
 from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 # Create your views here.
-def allProducts(request):
-    # complex dataset
-    products = Products.objects.all()
-    # convert to native python data type 
-    serialized = ProductsSerializer(products,many=True)
-    # convert to json data
-    jsonData = JSONRenderer().render(serialized.data)
-    return HttpResponse(jsonData,content_type='application/json')
 
 
-def singleProduct(request,title):
-    # query set
-    querySet = Products.objects.get(title=title)
-    # convert to native python
-    serialized = ProductsSerializer(querySet)
-    # convert to json 
-    data = JSONRenderer().render(serialized.data)
-    return HttpResponse(data,content_type='application/json')
+
+# this is api view
+@api_view(['GET','POST','PATCH','DELETE'])
+def fetchProducts(request,title=None):
+    if request.method=='GET':
+        if title is not None:
+            query  = Products.objects.get(title=title)
+            serializer = ProductsSerializer(query)
+            return Response(serializer.data,status=200)
+        data = Products.objects.all()
+        serializer = ProductsSerializer(data,many=True)
+        return Response(serializer.data,status=200)
+    
+    if request.method=='POST':
+        result = ProductsSerializer(data=request.data)
+        if result.is_valid():
+            result.save()
+            return Response(result.data,status=201)
+        return Response({'msg':"Failed"},status=400)
+    
+    if request.method=='PATCH':
+        queryData = Products.objects.get(title=title)
+        result = ProductsSerializer(queryData,data=request.data,partial=True)
+        if result.is_valid():
+            result.save()
+            return Response(result.data,status=200)
+        return Response({'msg':'Failed to update'},status=400)
+    
+    if request.method=='DELETE':
+        queryData = Products.objects.get(title=title)
+        
+        queryData.delete()
+        return Response({'msg':'deleted'})
+    
+    
+    
+
+
+# def allProducts(request):
+#     # complex dataset
+#     products = Products.objects.all()
+#     # convert to native python data type 
+#     serialized = ProductsSerializer(products,many=True)
+#     # convert to json data
+#     jsonData = JSONRenderer().render(serialized.data)
+#     return HttpResponse(jsonData,content_type='application/json')
+
+
+# def singleProduct(request,title):
+#     # query set
+#     querySet = Products.objects.get(title=title)
+#     # convert to native python
+#     serialized = ProductsSerializer(querySet)
+#     # convert to json 
+#     data = JSONRenderer().render(serialized.data)
+#     return HttpResponse(data,content_type='application/json')
 
 # @csrf_exempt
 # def create_product(request):
